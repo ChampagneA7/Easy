eb59329b985d2c0d54e78afd409fb66621b6806f
早睡 <1872200548@qq.com>
2023-05-27 13:05:19 +0800

    init Reggie

---
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -0,0 +1,205 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.OrdersDto;
+import com.itheima.reggie.entity.OrderDetail;
+import com.itheima.reggie.entity.Orders;
+import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.service.OrderDetailService;
+import com.itheima.reggie.service.OrdersService;
+import com.itheima.reggie.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    private OrdersService ordersService;
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/submit")
+    public R<String> submit(@RequestBody Orders orders) {
+
+        log.info("订单数据：{}",orders);
+
+        ordersService.submit(orders);
+
+        return R.success("下单成功");
+    }
+
+    /**
+     * 后台分页查询订单
+     * @param page
+     * @param pageSize
+     * @param number
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String number, String beginTime, String endTime) {
+        //分页构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Orders> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件,根据订单时间进行排序
+        lambdaQueryWrapper.orderByDesc(Orders::getOrderTime);
+
+        //打印订单号
+        log.info("订单号：{}",number);
+
+        //添加订单号比较条件
+        lambdaQueryWrapper.eq(number!=null,Orders::getNumber,number);
+
+
+        //如果 开始时间 和 结束时间 都不为空
+        if(beginTime != null && endTime != null){
+            //SQL : where (orderTime BETWEEN beginTime AND endTime)
+            //添加范围内查询条件 筛选出下单时间 大于等于 开始时间 小于等于 结束时间
+            lambdaQueryWrapper.between(Orders::getOrderTime,beginTime,endTime);
+        }
+
+        //进行分页查询
+        ordersService.page(pageInfo, lambdaQueryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     *  更改订单状态
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    public R<String> status(@RequestBody Orders orders) {
+
+        log.info(orders.toString());
+
+        ordersService.updateById(orders);
+
+        return R.success("派送成功");
+    }
+
+    /**
+     * 用户订单
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/userPage")
+    public R<Page> userPage(int page , int pageSize) {
+
+        //分页构造器
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+        Page<OrdersDto> ordersDtoPage = new Page<>();
+
+        //SQL：select * from orders where user_id = ? order by order_time desc
+
+        //条件构造器
+        LambdaQueryWrapper<Orders> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加用户ID
+        lambdaQueryWrapper.eq(Orders::getUserId, BaseContext.getCurrentId());
+        //添加排序条件,根据订单时间进行倒序
+        lambdaQueryWrapper.orderByDesc(Orders::getOrderTime);
+
+        //进行分页查询
+        ordersService.page(pageInfo,lambdaQueryWrapper);
+
+        //对象拷贝 将pageInfo 拷贝 到 orderDtoPage 忽略 records
+        BeanUtils.copyProperties(pageInfo,ordersDtoPage,"records");
+
+        //获取pageInfo的records
+        List<Orders> records = pageInfo.getRecords();
+
+        //遍历records
+        List<OrdersDto> list = records.stream().map((item) -> {
+
+            OrdersDto ordersDto = new OrdersDto();
+            //将 records的数据拷贝到 ordersDto
+            BeanUtils.copyProperties(item,ordersDto);
+
+            //SQL：select * from order_detail where order_id = ?
+
+            //条件构造器
+            LambdaQueryWrapper<OrderDetail> queryWrapper = new LambdaQueryWrapper<>();
+            //通过records里面的订单ID [number] 对比 订单明细表的order_id 查询订单明细
+            queryWrapper.eq(OrderDetail::getOrderId,item.getNumber());
+            //查询出订单明细信息 列表
+            List<OrderDetail> orderDetail = orderDetailService.list(queryWrapper);
+
+            ordersDto.setOrderDetails(orderDetail);
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        ordersDtoPage.setRecords(list);
+
+        return R.success(ordersDtoPage);
+    }
+
+    /**
+     * 再来一单
+     * @param orders
+     * @return
+     */
+    @PostMapping("/again")
+    public R<String> again(@RequestBody Orders orders) {
+        log.info("订单：{}",orders);
+
+        //获取订单ID
+        //SQL : select * from orders where id = ?
+        Orders ordersData = ordersService.getById(orders.getId());
+        String id = ordersData.getNumber();
+
+        //通过订单ID查询订单明细
+        //SQL : select * from order_detail where order_id = ?
+        LambdaQueryWrapper<OrderDetail> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(OrderDetail::getOrderId,id);
+        List<OrderDetail> orderDetailList = orderDetailService.list(lambdaQueryWrapper);
+        log.info("该订单数据：{}",orderDetailList);
+
+        //清空现在的购物车
+        //SQL : delete * from shopping_cart where user_id = ?
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        shoppingCartService.remove(queryWrapper);
+
+        //将订单明细里的数据添加到购物车
+        List<ShoppingCart> shoppingCartList = orderDetailList.stream().map((item) -> {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.setName(item.getName());
+            shoppingCart.setImage(item.getImage());
+            shoppingCart.setUserId(BaseContext.getCurrentId());
+            shoppingCart.setDishId(item.getDishId());
+            shoppingCart.setSetmealId(item.getSetmealId());
+            shoppingCart.setDishFlavor(item.getDishFlavor());
+            shoppingCart.setNumber(item.getNumber());
+            shoppingCart.setAmount(item.getAmount());
+            return shoppingCart;
+        }).collect(Collectors.toList());
+
+        shoppingCartService.saveBatch(shoppingCartList);
+
+        return R.success("Success！");
+    }
+}
---
+++ src/main/java/com/itheima/reggie/controller/SetmealController.java
@@ -0,0 +1,180 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.SetmealDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.SetmealDishService;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/setmeal")
+@Slf4j
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+
+    /**
+     * 新增套餐
+     * @param setmealDto
+     * @return
+     */
+    @PostMapping()
+    public R<String> save(@RequestBody SetmealDto setmealDto){
+        log.info("套餐信息：{}",setmealDto);
+
+        setmealService.saveWithDish(setmealDto);
+
+        return R.success("新增套餐成功");
+    }
+
+    /**
+     * 套餐信息分页
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        //构造分页构造器
+        Page<Setmeal> pageInfo = new Page<>(page,pageSize);
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        //添加查询条件，根据name进行like模糊查询
+        queryWrapper.like(name != null,Setmeal::getName,name);
+        //添加排序条件，根据更新时间降序排序
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        //执行分页查询
+        setmealService.page(pageInfo,queryWrapper);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,setmealDtoPage,"records");
+        List<Setmeal> records = pageInfo.getRecords();
+
+        List<SetmealDto> list = records.stream().map((item) ->{
+            SetmealDto setmealDto = new SetmealDto();
+            //对象拷贝
+            BeanUtils.copyProperties(item,setmealDto);
+            //分类ID
+            Long categoryId = item.getCategoryId();
+            //根据分类ID查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category != null){
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        setmealDtoPage.setRecords(list);
+
+        return R.success(setmealDtoPage);
+    }
+
+    /**
+     * 删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(@RequestParam List<Long> ids){
+        log.info("ids:{}",ids);
+
+        setmealService.removeWithDish(ids);
+
+        return R.success("套餐数据删除成功");
+    }
+
+    /**
+     * 修改套餐停启售状态
+     * @param status
+     * @param ids
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    public R<String> status(@PathVariable Integer status ,@RequestParam List<Long> ids){
+        log.info("状态为：{}，id为：{}",status,ids);
+
+        // list<Long> --> list<Setmeal>
+        List<Setmeal> setmealList = ids.stream()
+                .map(id -> {
+                    Setmeal setmeal = new Setmeal();
+                    setmeal.setId(id);
+                    setmeal.setStatus(status);
+                    return setmeal;
+                })
+                .collect(Collectors.toList());
+
+        //批量更新套餐状态
+        setmealService.updateBatchById(setmealList);
+
+        return R.success("修改套餐状态成功");
+    }
+
+    /**
+     * 根据ID查询套餐信息和对应的菜品信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<SetmealDto> get(@PathVariable Long id){
+
+        SetmealDto setmealDto = setmealService.getByIdWithDish(id);
+
+        return R.success(setmealDto);
+    }
+
+    /**
+     * 修改套餐
+     * @param setmealDto
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody SetmealDto setmealDto) {
+        log.info(setmealDto.toString());
+
+        setmealService.updateWithDish(setmealDto);
+
+        return R.success("修改套餐成功");
+    }
+
+    /**
+     * 根据条件查询套餐数据
+     * @param setmeal
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Setmeal>> list(Setmeal setmeal){
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
+        queryWrapper.eq(setmeal.getStatus() != null,Setmeal::getStatus,setmeal.getStatus());
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        List<Setmeal> list = setmealService.list(queryWrapper);
+
+        return R.success(list);
+    }
+}
